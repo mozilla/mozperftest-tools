a26f594c579b95dae5ffa2fa76c436088236ca3c
gmierz <gmierz1@live.ca>
2020-06-17 22:40:27 -0400

    Reformat file.

--- fenix-retrieval/generate_applink_data.py
+++ fenix-retrieval/generate_applink_data.py
@@ -20,35 +20,35 @@ except ImportError:
 
 RETRY_SLEEP = 10
 AD_QUERY = {
-	"from":"task",
-	"where":{"and":[
-		{"regex":{"run.name":".*fenix.*"}},
-		{"regex":{"run.name":".*perftest.*"}},
-		{"in":{"repo.branch.name":["mozilla-central"]}}
-	]},
-	"select":["task.artifacts", "action.start_time", "task.id"],
-	"limit":100000
+    "from":"task",
+    "where":{"and":[
+        {"regex":{"run.name":".*fenix.*"}},
+        {"regex":{"run.name":".*perftest.*"}},
+        {"in":{"repo.branch.name":["mozilla-central"]}}
+    ]},
+    "select":["task.artifacts", "action.start_time", "task.id"],
+    "limit":100000
 }
 
 
 def csv_generation_parser():
-	"""Parser for the CSV generation script."""
-	parser = argparse.ArgumentParser("Run this tool to build CSVs containing Fenix data from some tasks " +
-									 "running with the multi-commit paradigm in mozilla-central " +
-									 "(must have perfherder data).")
-	parser.add_argument("-t", "--test-name", type=str, default="applink",
-						help="The name of the test to get data from (must exist in the task name). " +
-						"Defaults to `applink`.")
-	parser.add_argument("-d", "--device", type=str, choices=["p2", "g5"], default="p2",
-						help="Device to get data from.")
-	parser.add_argument("-c", "--cache-path", type=str, default=None,
-						help="Path to a cache for perfherder artifacts (so you don't re-download them). " +
-						"Disabled by default.")
-	parser.add_argument("-r", "--fenix-repo", type=str, required=True,
-						help="Path to a local Fenix github repo.")
-	parser.add_argument("-o", "--output", type=str, default=None,
-						help="Path to the output directory. Defaults to current working directory.")
-	return parser
+    """Parser for the CSV generation script."""
+    parser = argparse.ArgumentParser("Run this tool to build CSVs containing Fenix data from some tasks " +
+                                     "running with the multi-commit paradigm in mozilla-central " +
+                                     "(must have perfherder data).")
+    parser.add_argument("-t", "--test-name", type=str, default="applink",
+                        help="The name of the test to get data from (must exist in the task name). " +
+                        "Defaults to `applink`.")
+    parser.add_argument("-d", "--device", type=str, choices=["p2", "g5"], default="p2",
+                        help="Device to get data from.")
+    parser.add_argument("-c", "--cache-path", type=str, default=None,
+                        help="Path to a cache for perfherder artifacts (so you don't re-download them). " +
+                        "Disabled by default.")
+    parser.add_argument("-r", "--fenix-repo", type=str, required=True,
+                        help="Path to a local Fenix github repo.")
+    parser.add_argument("-o", "--output", type=str, default=None,
+                        help="Path to the output directory. Defaults to current working directory.")
+    return parser
 
 
 def query_activedata(query_json):
@@ -101,106 +101,107 @@ def download_file(url, target, retry_sleep=RETRY_SLEEP, attempts=3):
 
 
 def build_csv(
-		fenix_repo,
-		test_name="applink",
-		device_name="p2",
-		output=None,
-		cache_path=None
-	):
-	"""Generates a CSV file containing per-commit fenix data
-	for a given test name.
-	"""
-	if cache_path:
-		cache_path = pathlib.Path(cache_path)
-		cache_path.mkdir(parents=True, exist_ok=True)
-	else:
-		cache_path = tempfile.mkdtemp()
+        fenix_repo,
+        test_name="applink",
+        device_name="p2",
+        output=None,
+        cache_path=None
+    ):
+    """Generates a CSV file containing per-commit fenix data
+    for a given test name.
+    """
+    if cache_path:
+        cache_path = pathlib.Path(cache_path)
+        cache_path.mkdir(parents=True, exist_ok=True)
+    else:
+        cache_path = tempfile.mkdtemp()
 
-	if output:
-		output = pathlib.Path(output)
-		output.mkdir(parents=True, exist_ok=True)
-	else:
-		output = pathlib.Path(".")
+    if output:
+        output = pathlib.Path(output)
+        output.mkdir(parents=True, exist_ok=True)
+    else:
+        output = pathlib.Path(".")
 
-	# Initialize the git directory now before the long steps below
-	fenix = git.Repo(fenix_repo)
+    # Initialize the git directory now before the long steps below
+    fenix = git.Repo(fenix_repo)
 
-	print(f"Generating data for {test_name} on the {device_name} device...")
+    print(f"Generating data for {test_name} on the {device_name} device...")
 
-	## Get the AD data
-	AD_QUERY["where"]["and"].extend([
-		{"regex": {"run.name": ".*%s.*" % test_name}},
-		{"regex": {"run.name": ".*-%s-.*" % device_name}}
-	])
-	data = query_activedata(AD_QUERY)
+    ## Get the AD data
+    AD_QUERY["where"]["and"].extend([
+        {"regex": {"run.name": ".*%s.*" % test_name}},
+        {"regex": {"run.name": ".*-%s-.*" % device_name}}
+    ])
+    data = query_activedata(AD_QUERY)
 
-	allph = []
-	for c, artifacts in enumerate(data["task.artifacts"]):
-		if not artifacts: continue
+    allph = []
+    for c, artifacts in enumerate(data["task.artifacts"]):
+        if not artifacts: continue
 
-		for artifact in artifacts:
-			if not artifact: continue
+        for artifact in artifacts:
+            if not artifact: continue
 
-			ph = artifact["url"].split("/")[-1]
-			if "perfherder" not in ph or ph.startswith("perfherder"): continue
+            ph = artifact["url"].split("/")[-1]
+            if "perfherder" not in ph or ph.startswith("perfherder"): continue
 
-			allph.append(
-				(artifact["url"], ph, ph.split("-")[0], data["task.id"][c])
-			)
-	
-	## Download the perfherder data and get its commit date
-	nallph = []
-	for url, file, rev, taskid in allph:
-		file = f"{taskid}-{test_name}-{device_name}-{file}"
-		fp = pathlib.Path(cache_path, file)
-		if not fp.exists():
-			print(f"Downloading to {fp}" )
-			download_file(url, fp)
-		with fp.open() as f:
-			phd = json.load(f)
+            allph.append(
+                (artifact["url"], ph, ph.split("-")[0], data["task.id"][c])
+            )
+    
+    ## Download the perfherder data and get its commit date
+    nallph = []
+    for url, file, rev, taskid in allph:
+        file = f"{taskid}-{test_name}-{device_name}-{file}"
+        fp = pathlib.Path(cache_path, file)
+        if not fp.exists():
+            print(f"Downloading to {fp}" )
+            download_file(url, fp)
+        with fp.open() as f:
+            phd = json.load(f)
 
-		# sanity checks
-		if ("suites" not in phd or
-			len(phd["suites"]) == 0 or
-			"value" not in phd["suites"][0]):
-			print("Bad data, skipping...")
-			continue
+        # sanity checks
+        if ("suites" not in phd or
+            len(phd["suites"]) == 0 or
+            "value" not in phd["suites"][0]):
+            print("Bad data, skipping...")
+            continue
 
-		try:
-			nallph.append((fenix.commit(rev).committed_date, phd["suites"][0]["value"], rev))
-		except ValueError:
-			# Some commits don't exist which is really weird - I don't
-			# understand how there's a build for them when we can't find them
-			# in the fenix repo.
-			print("Failed to find an actual commit for %s" % rev)
+        try:
+            nallph.append((fenix.commit(rev).committed_date, phd["suites"][0]["value"], rev))
+        except ValueError:
+            # Some commits don't exist which is really weird - I don't
+            # understand how there's a build for them when we can't find them
+            # in the fenix repo.
+            print("Failed to find an actual commit for %s" % rev)
 
-	# Sort the data by time
-	allphs = sorted(nallph, key=lambda x: x[0])
+    # Sort the data by time
+    allphs = sorted(nallph, key=lambda x: x[0])
 
-	## Store as a CSV
-	csvfile = pathlib.Path(output, f"{test_name}-{device_name}.csv")
-	with csvfile.open("w") as f:
-		writer = csv.writer(f)
-		writer.writerow(["times", "data", "revision"])
-		writer.writerows(allphs)
-	print(f"Finished generation. Data contained in {str(csvfile)}")
+    ## Store as a CSV
+    csvfile = pathlib.Path(output, f"{test_name}-{device_name}.csv")
+    with csvfile.open("w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["times", "data", "revision"])
+        writer.writerows(allphs)
+    print(f"Finished generation. Data contained in {str(csvfile)}")
 
-	try:
-		from matplotlib import pyplot as plt
-		plt.figure()
-		plt.plot([v[0] for v in allphs], [v[1] for v in allphs])
-		plt.scatter([v[0] for v in allphs], [v[1] for v in allphs])
-		plt.show()
-	except ImportError:
-		print("Skipping print stage, cannot find matplotlib")
-		return
+    try:
+        from matplotlib import pyplot as plt
+        plt.figure()
+        plt.plot([v[0] for v in allphs], [v[1] for v in allphs])
+        plt.scatter([v[0] for v in allphs], [v[1] for v in allphs])
+        plt.show()
+    except ImportError:
+        print("Skipping print stage, cannot find matplotlib")
+        return
+
 
 if __name__=="__main__":
-	args = csv_generation_parser().parse_args()
-	build_csv(
-		args.fenix_repo,
-		args.test_name,
-		args.device,
-		args.output,
-		args.cache_path
-	)
+    args = csv_generation_parser().parse_args()
+    build_csv(
+        args.fenix_repo,
+        args.test_name,
+        args.device,
+        args.output,
+        args.cache_path
+    )
