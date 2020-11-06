5a395994352ffa15200bac6b0abff5e27274b26a
gmierz <gmierz1@live.ca>
2020-11-06 15:05:54 -0500

    Add browsertime comparison script and update README.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # moz-current-tests
 
-This code can be used to determine where all tests are running, what tests are running on which platform or what platforms are running which tests. It is produced from a given `full-task-graph.json` artifact.
+This repository is a collection of various tools that are useful for the things we do in Performance and Performance Testing. You can find the most interesting ones documented below.
+
+## Generating a Test Report
+
+The code in `generate_test_report.py` can be used to determine where all tests are running, what tests are running on which platform or what platforms are running which tests. It is produced from a given `full-task-graph.json` artifact.
 
 Sample command: `python3 generate_test_report.py --tests raptor gecko --platform-breakdown --match-all-tests --ignore-no-projects`
 This will print out all raptor gecko tests and where they are running broken down by platform:
@@ -125,3 +129,49 @@ test-android-hw-p2-8-0-arm7-api-16/opt
 ```
 
 Run `python3 generate_test_report.py --help` for more options.
+
+
+## Browsertime Side-by-Side Video Comparisons
+
+The `generate_side_by_side.py` script can be used to generate a side-by-side comparion of two browsertime videos. This can be useful for determining if a regression/improvement is legitimate or not. It uses the similarity metric which is calculated using video histograms. When 
+
+```
+$ python3 generate_side_by_side.py --help
+usage: This tool can be used to generate a side-by-side visualization of two videos.
+
+ When using this tool, make sure that the `--test-name` is an exact match, i.e. if you are comparing  the task `test-linux64-shippable-qr/opt-browsertime-tp6-firefox-linkedin-e10s` between two revisions, then use `browsertime-tp6-firefox-linkedin-e10s` as the suite name and `test-linux64-shippable-qr/opt` as the platform.
+
+ The side-by-side video produced will be found in the output folder. The video on the left-hand side of the screen is the old/base video, and the video on the right-hand side of the screen is the new video.
+       [-h] --base-revision BASE_REVISION [--base-branch BASE_BRANCH]
+       --new-revision NEW_REVISION [--new-branch NEW_BRANCH] --test-name
+       TEST_NAME --platform PLATFORM [--overwrite] [--skip-download]
+       [--output OUTPUT]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --base-revision BASE_REVISION
+                        The base revision to compare a new revision to.
+  --base-branch BASE_BRANCH
+                        Branch to search for the base revision.
+  --new-revision NEW_REVISION
+                        The base revision to compare a new revision to.
+  --new-branch NEW_BRANCH
+                        Branch to search for the new revision.
+  --test-name TEST_NAME
+                        The name of the test task to get videos from.
+  --platform PLATFORM   Platforms to return results for. Defaults to all.
+  --overwrite           If set, the downloaded task group data will be deleted
+                        before it gets re-downloaded.
+  --skip-download       If set, we won't try to download artifacts again and
+                        we'll try using what already exists in the output
+                        folder.
+  --output OUTPUT       This is where the data will be saved. Defaults to CWD.
+                        You can include a name for the file here, otherwise it
+                        will default to side-by-side.mp4.
+```
+
+## Other tools
+
+There are other useful tools in this repo as well. For instance, the `high-value-tests` folder contains logic for determining the what tests are the most valuable given a set of alerts and also produces a minimized list of tests that could catch all alerts.
+
+
---
+++ generate_side_by_side.py
@@ -0,0 +1,369 @@
+#!/usr/bin/python3
+"""
+Used to produce comparisons of browsertime videos between a base
+and a new revision.
+"""
+import argparse
+import cv2
+import gc
+import numpy as np
+import os
+import pathlib
+import json
+import shutil
+import subprocess
+
+from matplotlib import pyplot as plt
+from scipy.stats import spearmanr
+from sys import stdout
+from time import sleep
+
+try:
+    from urllib.parse import urlencode
+    from urllib.request import urlopen, urlretrieve
+except ImportError:
+    from urllib import urlencode, urlretrieve
+    from urllib2 import urlopen
+
+from artifact_downloader import artifact_downloader
+from task_processor import get_task_data_paths
+
+
+TASK_IDS = \
+    "https://firefox-ci-tc.services.mozilla.com/api/index/v1/tasks/" + \
+    "gecko.v2.{}.revision.{}.firefox"
+
+TASK_INFO = "https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/{}"
+
+
+def side_by_side_parser():
+    parser = argparse.ArgumentParser(
+        "This tool can be used to generate a side-by-side visualization of two videos. "
+        "When using this tool, make sure that the `--test-name` is an exact match, i.e. "
+        "if you are comparing  the task `test-linux64-shippable-qr/opt-browsertime-tp6-firefox-linkedin-e10s` "
+        "between two revisions, then use `browsertime-tp6-firefox-linkedin-e10s` as the suite name "
+        "and `test-linux64-shippable-qr/opt` as the platform."
+    )
+    parser.add_argument("--base-revision", type=str, required=True,
+                        help="The base revision to compare a new revision to.")
+    parser.add_argument("--base-branch", type=str, default="autoland",
+                        help="Branch to search for the base revision.")
+    parser.add_argument("--new-revision", type=str, required=True,
+                        help="The base revision to compare a new revision to.")
+    parser.add_argument("--new-branch", type=str, default="autoland",
+                        help="Branch to search for the new revision.")
+    parser.add_argument("--test-name", type=str, required=True,
+                        help="The name of the test task to get videos from.")
+    parser.add_argument("--platform", type=str, required=True,
+                        help="Platforms to return results for. Defaults to all.")
+    parser.add_argument("--overwrite", action="store_true", default=False,
+                        help="If set, the downloaded task group data will be deleted before " +
+                        "it gets re-downloaded.")
+    parser.add_argument("--skip-download", action="store_true", default=False,
+                        help="If set, we won't try to download artifacts again and we'll " +
+                        "try using what already exists in the output folder.")
+    parser.add_argument("--output", type=str, default=os.getcwd(),
+                        help="This is where the data will be saved. Defaults to CWD. " +
+                        "You can include a name for the file here, otherwise it will " +
+                        "default to side-by-side.mp4.")
+    return parser
+
+
+def write_same_line(x, sleep_time=0.0001):
+    stdout.write("\r%s" % str(x))
+    stdout.flush()
+    sleep(sleep_time)
+
+
+def finish_same_line():
+    stdout.write("\r  \r\n")
+
+
+def get_json(url, params=None):
+    if params is not None:
+        url += "?" + urlencode(params)
+
+    r = urlopen(url).read().decode("utf-8")
+
+    return json.loads(r)
+
+
+def find_task_group_id(revision):
+    # Find a task ID from this revision first
+    task_ids_url = TASK_IDS.format(revision)
+
+    print("Downloading task ids from: %s" % task_ids_url)
+    task_ids_data = get_json(task_ids_url)
+    if "tasks" not in task_ids_data or len(task_ids_data["tasks"]) == 0:
+        raise Exception("Cannot find any task IDs for %s!" % revision)
+
+    task_id = task_ids_data["tasks"][0]["taskId"]
+
+    # Find the task group ID now
+    task_group_url = TASK_INFO.format(task_id)
+    print("Downloading task group id from: %s" % task_group_url)
+    task_info = get_json(task_group_url)
+
+    return task_info["taskGroupId"]
+
+
+def find_videos(artifact_dir):
+    # Find the cold/warm browsertime.json files
+    cold_path = ""
+    warm_path = ""
+    for path in pathlib.Path(artifact_dir).rglob("*-browsertime.json"):
+        if "cold" in str(path):
+            cold_path = path
+        elif "warm" in str(path):
+            warm_path = path
+    if not cold_path:
+        raise Exception("Cannot find a browsertime.json file for the cold pageloads.")
+    if not warm_path:
+        raise Exception("Cannot find a browsertime.json file for the warm pageloads.")
+
+    with cold_path.open() as f:
+        cold_data = json.load(f)
+    with warm_path.open() as f:
+        warm_data = json.load(f)
+
+    return {
+        "cold": [
+            str(pathlib.Path(cold_path.parents[0], file))
+            for file in cold_data[0]["files"]["video"]
+        ],
+        "warm": [
+            str(pathlib.Path(warm_path.parents[0], file))
+            for file in warm_data[0]["files"]["video"]
+        ],
+    }
+
+
+def get_similarity(old_videos_info, new_videos_info, output, prefix=""):
+    """Calculates a similarity score for two groupings of videos.
+
+    The technique works as follows:
+        1. Get the last live site test.
+        2. For each 15x15 video pairings, build a cross-correlation matrix:
+            1. Get each of the videos and calculate their histograms
+               across the full videos.
+            2. Calculate the correlation coefficient between these two.
+        3. Average the cross-correlation matrix to obtain the score.
+
+    The 2D similarity score is the same, except that it builds a histogram
+    from the final frame instead of the full video.
+
+    Args:
+        old_videos: List of old videos.
+        new_videos: List of new videos (from this task).
+        output: Location to output videos with low similarity scores.
+        prefix: Prefix a string to the output.
+    Returns:
+        Two similarity scores (3D, 2D) as a float.
+    """
+
+    def _get_frames(video):
+        """Gets all frames from a video into a list."""
+        allframes = []
+        while video.isOpened():
+            ret, frame = video.read()
+            if ret:
+                # Convert to gray to simplify the process
+                allframes.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+            else:
+                video.release()
+                break
+        return allframes
+
+    nhists = []
+
+    old_videos = [entry["data"] for entry in old_videos_info]
+    new_videos = [entry["data"] for entry in new_videos_info]
+
+    total_vids = min(len(old_videos), len(new_videos))
+    xcorr = np.zeros((total_vids, total_vids))
+
+    for i in range(total_vids):
+        datao = np.asarray(_get_frames(old_videos[i]))
+
+        histo, _, _ = plt.hist(datao.flatten(), bins=255)
+
+        for j in range(total_vids):
+            write_same_line("Comparing old video %s to new video %s" % (i+1, j+1))
+            if i == 0:
+                # Only calculate the histograms once; it takes time
+                datan = np.asarray(_get_frames(new_videos[j]))
+
+                histn, _, _ = plt.hist(datan.flatten(), bins=255)
+
+                nhists.append(histn)
+            else:
+                histn = nhists[j]
+
+            rho, _ = spearmanr(histn, histo)
+
+            xcorr[i, j] = rho
+
+    finish_same_line()
+
+    similarity = np.nanmean(xcorr)
+
+    print("Average 3D similarity: %s" % str(np.round(similarity, 5)))
+
+    minind = np.unravel_index(np.argmin(xcorr, axis=None), xcorr.shape)
+
+    oldvid = old_videos_info[minind[0]]["path"]
+    oldvidnewpath = str(pathlib.Path(output, "%sold_video.mp4" % prefix))
+    shutil.copyfile(oldvid, oldvidnewpath)
+
+    newvid = new_videos_info[minind[1]]["path"]
+    newvidnewpath = str(pathlib.Path(output, "%snew_video.mp4" % prefix))
+    shutil.copyfile(newvid, newvidnewpath)
+
+    return {
+        "sim3": np.round(similarity, 5),
+        "oldvid": oldvidnewpath,
+        "newvid": newvidnewpath
+    }
+
+
+def find_lowest_similarity(base_videos, new_videos, output, prefix):
+    def _open_data(file):
+        return cv2.VideoCapture(str(file))
+    return get_similarity(
+        [
+            {"data": _open_data(str(f)), "path": str(f)}
+            for f in base_videos
+        ],
+        [
+            {"data": _open_data(str(f)), "path": str(f)}
+            for f in new_videos
+        ],
+        output,
+        prefix
+    )
+
+
+def build_side_by_side(base_video, new_video, output):
+    subprocess.check_output([
+        "ffmpeg",
+        "-i", str(base_video),
+        "-i", str(new_video),
+        "-filter_complex", "[0:v]pad=iw*2:ih[int];[int][1:v]overlay=W/2:0[vid]",
+        "-map", "[vid]",
+        "-c:v", "libx264",
+        "-crf", "23",
+        "-preset", "veryfast",
+        str(output)
+    ])
+
+
+if __name__=="__main__":
+    args = side_by_side_parser().parse_args()
+    overwrite = args.overwrite
+
+    if shutil.which("ffmpeg") is None:
+        raise Exception("Cannot find ffmpeg in path! Please install it before continuing.")
+
+    # Parse the given output argument
+    filename = "side-by-side.mp4"
+    output = pathlib.Path(args.output)
+    if output.exists() and output.is_file():
+        print("Deleting existing output file...")
+        output.unlink()
+    elif not output.suffixes:
+        output.mkdir(parents=True, exist_ok=True)
+    else:
+        filename = output.name
+        output = output.parents[0]
+        output.mkdir(parents=True, exist_ok=True)
+
+    # Get the task group IDs for the revisions
+    base_revision_id = find_task_group_id(args.base_revision, args.base_branch)
+    new_revision_id = find_task_group_id(args.new_revision, args.new_branch)
+    if overwrite:
+        for task_dir in (
+            pathlib.Path(output, base_revision_id),
+            pathlib.Path(output, new_revision_id),
+        ):
+            if task_dir.exists():
+                print("Removing existing task group folder: %s" % str(task_dir))
+                shutil.rmtree(str(task_dir))
+
+    # Download the artifacts
+    base_task_dir = str(pathlib.Path(output, base_revision_id))
+    new_task_dir = str(pathlib.Path(output, new_revision_id))
+    if not args.skip_download:
+        base_task_dir, _ = artifact_downloader(
+            base_revision_id,
+            output_dir=str(output),
+            test_suites=[args.test_name],
+            platform=args.platform,
+            artifact_to_get=["browsertime-results"],
+            unzip_artifact=True,
+            download_failures=True,
+            ingest_continue=False
+        )
+        new_task_dir, _ = artifact_downloader(
+            new_revision_id,
+            output_dir=str(output),
+            test_suites=[args.test_name],
+            platform=args.platform,
+            artifact_to_get=["browsertime-results"],
+            unzip_artifact=True,
+            download_failures=True,
+            ingest_continue=False
+        )
+
+    # Get the paths to the directory holding the artifacts
+    base_paths = list(get_task_data_paths(
+        base_revision_id,
+        str(output),
+        artifact="browsertime-results"
+    ).values())
+    new_paths = list(get_task_data_paths(
+        new_revision_id,
+        str(output),
+        artifact="browsertime-results"
+    ).values())
+
+    # Make sure we only downloaded one task
+    if not base_paths or len(base_paths) > 1:
+        raise Exception(
+            "Too many artifacts downloaded for %s, can't compare!" % base_revision
+        )
+    if not new_paths or len(new_paths) > 1:
+        raise Exception(
+            "Too many artifacts downloaded for %s, can't compare!" % new_revision
+        )
+
+    # Gather the videos and split them between warm and cold
+    base_videos = find_videos(base_paths[0][0])
+    new_videos = find_videos(new_paths[0][0])
+
+    # Find the worst video pairing for cold and warm
+    print("Starting comparisons, this may take a few minutes")
+    print("Running comparison for cold pageloads...")
+    cold_pairing = find_lowest_similarity(
+        base_videos["cold"],
+        new_videos["cold"],
+        str(output),
+        "cold_"
+    )
+
+    gc.collect()
+    print("Running comparison for warm pageloads...")
+    warm_pairing = find_lowest_similarity(
+        base_videos["warm"],
+        new_videos["warm"],
+        str(output),
+        "warm_"
+    )
+
+    # Build up the side-by-side comparisons now
+    output_name = str(pathlib.Path(output, "cold-" + filename))
+    build_side_by_side(cold_pairing["oldvid"], cold_pairing["newvid"], output_name)
+    print("Successfully built a side-by-side cold comparison: %s" % output_name)
+
+    output_name = str(pathlib.Path(output, "warm-" + filename))
+    build_side_by_side(warm_pairing["oldvid"], warm_pairing["newvid"], output_name)
+    print("Successfully built a side-by-side warm comparison: %s" % output_name)
