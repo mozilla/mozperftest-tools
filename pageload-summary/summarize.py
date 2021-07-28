added --end-date.   Use days for moving average period, not points.  Fix a divide-by-zero if there's no data.

pageload-summary/summarize.py
@@ -32,7 +32,7 @@ def summary_parser():
         "--moving-average-window",
         type=int,
         default=7,
-        help="Number of points to use for the moving average.",
+        help="Number of days to use for the moving average.",
     )
     parser.add_argument(
         "--by-site",
@@ -69,7 +69,12 @@ def summary_parser():
     parser.add_argument(
         "--start-date",
         type=datetime.datetime.fromisoformat,
-        help="Date to start analysis.",
+        help="Date to start analysis (inclusive).",
+    )
+    parser.add_argument(
+        "--end-date",
+        type=datetime.datetime.fromisoformat,
+        help="Date to end analysis (inclusive).",
     )
     parser.add_argument(
         "--app",
@@ -104,7 +109,7 @@ def get_data_ind(data, fieldname):
     return None
 
 
-def organize_data(data, platforms, platform_pattern, start_date, by_site = False, app_only=None):
+def organize_data(data, platforms, platform_pattern, start_date, end_date, by_site = False, app_only=None):
     """Organizes the data into a format that is easier to handle."""
 
     platform_ind = get_data_ind(data, "platform")
@@ -124,6 +129,8 @@ def organize_data(data, platforms, platform_pattern, start_date, by_site = False
             continue
         date = datetime.datetime.fromisoformat(entry[time_ind])
         if start_date != None and date < start_date:
+            continue
+        if end_date != None and date > end_date:
             continue
 
         test = entry[test_ind]
@@ -227,8 +234,8 @@ def temporal_aggregation(times, timespan=24):
     return aggr_times[::-1]
 
 
-def summarize(data, platforms, platform_pattern, timespan, moving_average_window, start_date, by_site, app_only):
-    org_data = organize_data(data, platforms, platform_pattern, start_date, by_site, app_only)
+def summarize(data, platforms, platform_pattern, timespan, moving_average_window, start_date, end_date, by_site, app_only):
+    org_data = organize_data(data, platforms, platform_pattern, start_date, end_date, by_site, app_only)
 
     summary = {}
 
@@ -263,13 +270,19 @@ def summarize(data, platforms, platform_pattern, timespan, moving_average_window
 
                     ma_vals = []
                     window = []
-                    if len(summarized_vals) > moving_average_window:
+                    time_window = []
+                    startdate = datetime.datetime.fromisoformat(summarized_vals[0][0])
+                    enddate = datetime.datetime.fromisoformat(summarized_vals[-1][0])
+                    if (enddate-startdate).days > moving_average_window:
                         for time, val in summarized_vals:
-                            if len(window) < moving_average_window:
-                                window.append(val)
-                            if len(window) == moving_average_window:
+                            window.append(val)
+                            time_window.append(time)
+                            startdate = datetime.datetime.fromisoformat(time_window[0])
+                            enddate = datetime.datetime.fromisoformat(time)
+                            if (enddate-startdate).days > moving_average_window:
                                 ma_vals.append((time, np.mean(window)))
                                 window = window[1:]
+                                time_window = time_window[1:]
                     else:
                         ma_vals = summarized_vals
 
@@ -401,6 +414,10 @@ def text_summary(summary, width=20, plat_width=50):
                     if len(data["moving_average"]) > 1:
                         prev = np.round(data["moving_average"][-2][1], 2)
 
+                    if prev > 0.0:
+                        delta = f" ({np.round(cur/prev,4)})";
+                    else:
+                        delta = " (NaN)"
                     lines.append(
                         format_line.format(
                             platform_str,
@@ -408,7 +425,7 @@ def text_summary(summary, width=20, plat_width=50):
                             variant_str,
                             pl_type,
                             prev,
-                            str(cur) + f" ({np.round(cur/prev,4)})",
+                            str(cur) + delta,
                             width=width,
                             plat_width=plat_width,
                         )
@@ -513,7 +530,7 @@ def main():
     # Process the data and visualize the results (after saving)
     data = open_csv_data(data_path)
 
-    results = summarize(data, args.platforms, args.platform_pattern, args.timespan, args.moving_average_window, args.start_date, args.by_site, args.app)
+    results = summarize(data, args.platforms, args.platform_pattern, args.timespan, args.moving_average_window, args.start_date, args.end_date, args.by_site, args.app)
     with pathlib.Path(output_folder, output_file).open("w") as f:
         json.dump(results, f)
 