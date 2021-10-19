Fix and clarify naming/logging in the variance analysis.

variance-analysis/variance_analysis.py
@@ -20,20 +20,21 @@ def run_variance_analysis(
         "google",
     ],
     platform="Unknown Platform",
+    groupings=None,
 ):
 
     if isinstance(data, str):
         with open(data) as f:
             data = json.load(f)
 
-    groupings = set()
+    tmp_groupings = set()
     subtests = set()
     grouped_data = {}
     for entry in data:
         tuned = "pooled"
         pltype = "warm"
         grouping = entry["name"]
-        groupings.add(grouping)
+        tmp_groupings.add(grouping)
         if "cold" in entry["subtest"]:
             pltype = "cold"
 
@@ -43,11 +44,12 @@ def run_variance_analysis(
 
         subtests.add(entry["subtest"])
 
+    if groupings is None:
+        groupings = sorted(list(tmp_groupings))
     if len(groupings) != 2:
         raise Exception(
             "Unable to compare variance. The number of groupings must be 2."
         )
-    groupings = list(groupings)
 
     def filter(data):
         # Apply a gaussian filter
@@ -66,78 +68,94 @@ def run_variance_analysis(
         plt.figure()
         plt.suptitle("%s Variance Analysis - %s" % (platform, pltype))
 
-        g5lvals = []
-        g5stds = []
+        levene_vals = []
+        std_dev_ratios = []
         for subtest in subtests:
-            p2t = grouped_data[pltype][groupings[0]].get(subtest, None)
-            p2wt = grouped_data[pltype][groupings[1]].get(subtest, None)
-            if p2t is not None and p2wt is not None:
-                p2t = filter(p2t)
-                p2wt = filter(p2wt)
-                lval, pv = stats.levene(p2t, p2wt)
-                if np.std(p2wt - np.mean(p2wt)) == 0:
+            group_1 = grouped_data[pltype][groupings[0]].get(subtest, None)
+            group_2 = grouped_data[pltype][groupings[1]].get(subtest, None)
+            if group_1 is not None and group_2 is not None:
+                group_1 = filter(group_1)
+                group_2 = filter(group_2)
+                lval, pv = stats.levene(group_1, group_2)
+                if np.std(group_2 - np.mean(group_2)) == 0:
                     continue
-                diff = np.std(p2t - np.mean(p2t)) / np.std(p2wt - np.mean(p2wt))
+                std_dev_ratio = np.std(group_1 - np.mean(group_1)) / np.std(
+                    group_2 - np.mean(group_2)
+                )
                 # Uncomment to check what the outliers look like
                 # if diff < 0.7:
                 #     print(subtest)
                 #     plt.figure()
                 #     plt.title(subtest)
-                #     print(len(p2t))
-                #     print(len([v for v in p2t if v < 1100]))
-                #     plt.scatter([1 for _ in p2t], p2t)
-                #     plt.scatter([2 for _ in p2wt], p2wt)
+                #     print(len(group_1))
+                #     print(len([v for v in group_1 if v < 1100]))
+                #     plt.scatter([1 for _ in group_1], group_1)
+                #     plt.scatter([2 for _ in group_2], group_2)
                 #     plt.ylabel("Time")
                 #     plt.xticks([1,2], ["chimera", "normal"])
                 #     plt.show()
                 #     continue
-                g5lvals.append(pv)
-                g5stds.append(diff)
+                levene_vals.append(pv)
+                std_dev_ratios.append(std_dev_ratio)
 
         plt.subplot(2, 1, 1)
-        plt.plot(g5stds)
+        plt.plot(std_dev_ratios)
         plt.title("Std. Dev.")
-        plt.ylabel("StdDev Diff - >1 is increased noise in group %s" % grouping[0])
+        plt.ylabel("StdDev Diff - >1 is increased noise in group %s" % groupings[0])
         plt.xlabel("Subtests")
         plt.subplot(2, 1, 2)
-        plt.plot(g5lvals)
+        plt.plot(levene_vals)
         plt.title("Levene's")
         plt.ylabel("P Value")
         plt.xlabel("Subtests")
 
-        g5lvals = np.asarray(g5lvals)
-        g5lvals = np.asarray([v for v in g5lvals if not np.isnan(v)])
-        g5stds = np.asarray([v for v in g5stds if not np.isnan(v)])
+        levene_vals = np.asarray(levene_vals)
+        levene_vals = np.asarray([v for v in levene_vals if not np.isnan(v)])
+        std_dev_ratios = np.asarray([v for v in std_dev_ratios if not np.isnan(v)])
         print(
             "%s - %s total: %s/%s"
             % (
                 platform,
                 pltype,
-                len(g5stds[np.where((g5lvals <= 0.05) & (np.isfinite(g5lvals)))]),
-                len(g5stds),
+                len(
+                    std_dev_ratios[
+                        np.where((levene_vals <= 0.05) & (np.isfinite(levene_vals)))
+                    ]
+                ),
+                len(std_dev_ratios),
             )
         )
         print(
             "Average noise diff: %s"
-            % np.mean(g5stds[np.where((g5lvals <= 0.05) & (np.isfinite(g5lvals)))])
+            % np.mean(
+                std_dev_ratios[
+                    np.where((levene_vals <= 0.05) & (np.isfinite(levene_vals)))
+                ]
+            )
         )
         print(
             "Significance of the average: %s"
-            % np.mean(g5lvals[np.where((g5lvals <= 0.05) & (np.isfinite(g5lvals)))])
+            % np.mean(
+                levene_vals[
+                    np.where((levene_vals <= 0.05) & (np.isfinite(levene_vals)))
+                ]
+            )
         )
 
-        sigs = g5stds[np.where((g5lvals <= 0.05) & (np.isfinite(g5lvals)))]
+        sigs = std_dev_ratios[
+            np.where((levene_vals <= 0.05) & (np.isfinite(levene_vals)))
+        ]
         sigsl = np.where(sigs < 1)[0]
         sigsh = np.where(sigs >= 1)[0]
-        print(f"Number of tests with lower noise: {len(sigsl)}")
-        print(f"Number of tests with higher noise: {len(sigsh)}")
+        print(f"Number of tests with lower noise in {groupings[0]}: {len(sigsl)}")
+        print(f"Number of tests with higher noise in {groupings[0]}: {len(sigsh)}")
 
         print(f"Averager decrease in noise for lower: {np.mean(sigs[sigsl])}")
         print(f"Average increase in noise for higher: {np.mean(sigs[sigsh])}")
         print(
-            "\nThe Average noise diff gives a ratio between the %s and the %s grouping. "
-            "A ratio greater than 1 implies that the noise is larger in %s"
-            % (groupings[0], groupings[1], groupings[0])
+            "\nThe Average noise diff gives a ratio between the "
+            f"{groupings[0]} and the {groupings[1]} grouping. "
+            f"A ratio greater than 1 implies that the noise is larger in {groupings[0]}."
         )
 
     plt.show()