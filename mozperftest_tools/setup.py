65bc4d35b2e8536913606b5c6e1c1f86b670d8a2
Alex Ionescu <aionescu@mozilla.com>
2022-06-09 13:28:01 +0300

    Handle task failures using the state.

--- mozperftest_tools/setup.py
+++ mozperftest_tools/setup.py
@@ -3,7 +3,7 @@ import sys
 import setuptools
 
 PACKAGE_NAME = "mozperftest_tools"
-PACKAGE_VERSION = "0.1.0"
+PACKAGE_VERSION = "0.1.1"
 
 # dependencies
 deps = ["matplotlib", "opencv-python", "requests"]
