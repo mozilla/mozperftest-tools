24543c987a08c182eb0bf395a7407704e55cb873
Gregory Mierzwinski <gmierz1@live.ca>
2023-02-21 11:28:55 -0500

    Add license info to the files.

--- generate_side_by_side.py
+++ generate_side_by_side.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 """
 Used to produce comparisons of browsertime videos between a base
 and a new revision.
 """
+
 import argparse
 import cv2
 import gc
