823f58ee1530fb4c488338fbbfe9acd1ce9b4aad
Gregory Mierzwinski <gmierz1@live.ca>
2023-03-14 08:22:39 -0400

    Update to v0.2.6.

--- mozperftest_tools/setup.py
+++ mozperftest_tools/setup.py
@@ -7,7 +7,7 @@ import sys
 import setuptools
 
 PACKAGE_NAME = "mozperftest_tools"
-PACKAGE_VERSION = "0.2.5"
+PACKAGE_VERSION = "0.2.6"
 HERE = pathlib.Path(__file__).parent.resolve()
 
 # dependencies
